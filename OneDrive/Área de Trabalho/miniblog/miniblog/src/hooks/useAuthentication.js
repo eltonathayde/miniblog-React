93c4b91e0bf7fc32e348a1a49c63ed925849f421
elton lima <eltonathayde2@gmail.com>
2022-05-27 12:42:11 -0300

    funcionalidade de login finalizada

--- OneDrive/Área de Trabalho/miniblog/miniblog/src/hooks/useAuthentication.js
+++ OneDrive/Área de Trabalho/miniblog/miniblog/src/hooks/useAuthentication.js
@@ -75,6 +75,35 @@ export const useAuthentication = () => {
     const logout = () => {
         checkIfIsCancelled();
         signOut(auth);
+    };
+
+
+    // login - sign 
+
+    const login  = async(data) => {
+         checkIfIsCancelled()
+
+        setLoading(true)
+        setError(false)
+
+        try {
+            await signInWithEmailAndPassword(auth,data.email, data.password)
+            setLoading(false);
+        } catch (error) {
+            
+            let systemErrorMessage
+            if(error.message.includes("user-not-found")){
+                systemErrorMessage = "Usuário não encontrado."
+            } else if (error.message.includes("wrong-password")){
+                systemErrorMessage = "Senha incorreta"
+            } else{
+                systemErrorMessage = "Ocorreu um erro, por favor tente mais tarde."
+            }
+
+            setError(systemErrorMessage)
+            setLoading(false);
+        }
+
     }
 
     useEffect(()=>{
@@ -87,5 +116,6 @@ export const useAuthentication = () => {
         error,
         loading,
         logout,
+        login
     };
 };
