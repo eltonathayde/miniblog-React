2a11184b369bebe786612b4be2b6f8c9c5a54a01
elton lima <eltonathayde2@gmail.com>
2022-05-30 14:19:18 -0300

    estrutura da pagina de home criada

--- OneDrive/Área de Trabalho/miniblog/miniblog/src/pages/Home/Home.js
+++ OneDrive/Área de Trabalho/miniblog/miniblog/src/pages/Home/Home.js
@@ -1,12 +1,29 @@
     
-   import styles from "./Home.module.css"
+//   CSS
+  import styles from "./Home.module.css"
+//   hooks
+ import{useNavigate, Link} from "react-router-dom"
+ import { useState } from "react"
 
+
+//  componentes
 
 
 const Home = () => {
-    return(
+    const[query,setQuery] = useState("");
+    const  handleSubmit = (e) => {
+        e.prevenDefault()
+    }
+        return(
         <div>
-            <h1>Home</h1>
+            <h1>Veja nosso posts mais recentes</h1>
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="Ou busque por tags..."  onChange={(e)=> setQuery(e.target.value)}/>
+                <button className="btn btn-dark">Pesquisar</button>
+            </form>
+            <div>
+                <h1>posts ...</h1>
+            </div>
       </div>
     )
 }
