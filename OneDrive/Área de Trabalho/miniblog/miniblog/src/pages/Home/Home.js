2b177697c307c2e7fc7af2a0ca34420bd0924360
elton lima <eltonathayde2@gmail.com>
2022-05-30 14:30:47 -0300

    estrutura da pagina home finalizada

--- OneDrive/Área de Trabalho/miniblog/miniblog/src/pages/Home/Home.js
+++ OneDrive/Área de Trabalho/miniblog/miniblog/src/pages/Home/Home.js
@@ -11,18 +11,29 @@
 
 const Home = () => {
     const[query,setQuery] = useState("");
+    const [posts] = useState([])
     const  handleSubmit = (e) => {
         e.prevenDefault()
     }
         return(
-        <div>
+        <div className={styles.home}>
             <h1>Veja nosso posts mais recentes</h1>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit} className={styles.search_form}>
                 <input type="text" placeholder="Ou busque por tags..."  onChange={(e)=> setQuery(e.target.value)}/>
                 <button className="btn btn-dark">Pesquisar</button>
             </form>
             <div>
                 <h1>posts ...</h1>
+                {posts && posts.length === 0 &&(
+                    <div className={styles.noposts}>
+                        <p>
+                            Não foram encontrados posts
+                        </p>
+                        <Link to="/posts/create" className="btn">
+                            Criar primeiro post
+                        </Link>
+                    </div>
+                )}
             </div>
       </div>
     )
