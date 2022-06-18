de2c89c62b040970b35376665b73d41b0bb0a7a8
elton lima <eltonathayde2@gmail.com>
2022-06-18 13:18:11 -0300

    estrutura da pagina de dashbord finalizada

--- OneDrive/Área de Trabalho/miniblog/miniblog/src/pages/Dashbord/Dashbord.js
+++ OneDrive/Área de Trabalho/miniblog/miniblog/src/pages/Dashbord/Dashbord.js
@@ -1,9 +1,32 @@
 import styles from "./Dashbord.module.css"
+import {Link} from "react-router-dom"
+
+// hooks
+
+import {useAuthValue} from '../../context/Authcontext'
+import {useFetchDocuments} from '../../hooks/useFetchDocuments'
 
 const Dashbord = () => {
+  const {user} = useAuthValue()
+  const uid = user.uid
+
+  // posts do usuario
+
+  const posts=[];
   return (
     <div>
-        <h1>Dashbord</h1>
+        <h2>Dashbord</h2>
+        <p>Gerencie os seus posts</p>
+        {posts && posts.length === 0 ? (
+       <div className={styles.noposts}>
+            <p>Não foram encontrados posts</p>
+            <Link to="/posts/create" className="btn">Criar primeiro post</Link>       
+            </div>
+        ) : (
+          <div>
+            <p>Tem posts</p>
+          </div>
+        ) }
     </div>
   )
 }
